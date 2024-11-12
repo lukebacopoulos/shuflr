54931ae613e87e12240e6e11bd858c3e2132e56c
lukebacopoulos <baco@bu.edu>
2024-11-12 18:34:41 -0500

    attempting deployment #3

---
+++ lib/authOptions.ts
@@ -0,0 +1,26 @@
+import SpotifyProvider from 'next-auth/providers/spotify'
+export const authOptions = {
+    providers: [
+        SpotifyProvider({
+            clientId: process.env.SPOTIFY_ID ?? "",
+            clientSecret: process.env.SPOTIFY_SECRET ?? "",
+            authorization: {
+                params: { scope: "user-top-read, user-library-read, user-modify-playback-state"},
+            },
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, account }) {
+            if (account) {
+                token.accessToken = account.access_token
+            }
+            return token
+        },
+        async session({ session, token }) {
+            session.accessToken = token.accessToken
+            return session
+        }
+        
+}
+
+}
